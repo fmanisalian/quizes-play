fc09a85f7807476c065959de511f73f624852836
Fernando Manisalián <fermancba@gmail.com>
2015-07-06 11:31:19 -0300

    Crear Comentarios

--- models/models.js
+++ models/models.js
@@ -28,7 +28,15 @@ var sequelize = new Sequelize(DB_name, user, pwd,
 
 //Importar la definición de la tabla Quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+//Importar definición de la tabla Comment en comment.js
+var Comment = sequelize.import(path.join(__dirname, 'comment'));
+
+//Establecer relaciones entre las tablas
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
 exports.Quiz = Quiz; //exportar definición de tabla Quiz
+exports.Comment = Comment;  //exportar definición de tabla Comment
 
 //sequelize.sync() crea e inicializa tabla de preguntas en DB
 sequelize.sync().then(function() {
