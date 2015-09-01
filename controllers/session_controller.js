f35293b61583513e82c93b2db3d6f082d5919c5f
Fernando Manisalián <fermancba@gmail.com>
2015-08-31 22:56:00 -0300

    Roles y Permisos

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -32,7 +32,7 @@ exports.create = function(req, res) {
 
         // Crear req.session.user y guardar campos id y username
         // La sesión se define por la existencia de: req.session.user       
-        req.session.user = {id:user.id, username:user.username}; 
+        req.session.user = {id:user.id, username:user.username, isAdmin:user.isAdmin}; 
         //Crear variable horaCaducaSesion:       
         req.session.horaCaducaSesion = (new Date()).getTime() + 120000;
 
